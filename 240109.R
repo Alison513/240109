2609a3583dad59bfab34cc6757be06e19b9e4afe
LAPTOP-4PD9IIMG\user <wuyishan0805@gmail.com>
2024-01-10 15:37:02 +0800

    新增數統4.123

--- 240109.R
+++ 240109.R
@@ -1,19 +1,22 @@
 #pnorm函數為常態分佈累積機率函數
-#4.85(a)
+#4.58(a)
 p1 = 1 - pnorm(1.2, mean = 0, sd = 1)
-p1
-#4.85(b)
+
+#4.58(b)
 p2= pnorm(0, mean = 0, sd = 1)-pnorm(-0.9, mean=0,sd=1)
 p2
-#4.85(c)
+#4.58(c)
 p3= pnorm(1.56, mean = 0, sd = 1)-pnorm(0.3, mean=0,sd=1)
 p3
-#4.85(d)
+#4.58(d)
 p4= pnorm(0.2, mean = 0, sd = 1)-pnorm(-0.2, mean=0,sd=1)
 p4
-#4.85(e)
+#4.58(e)
 p5= pnorm(-0.2, mean = 0, sd = 1)-pnorm(-1.56, mean=0,sd=1)
 p5
+ 
+
+
 
 #4.86(a)
 # 參數
@@ -34,3 +37,36 @@ p2
 
 #4.86(c)
 #the same result
+
+p1
+p2
+
+
+#4.123
+#(a)可得知f(y)～beta(4,3)且k為60
+#4.123(b)
+
+library(stats)
+
+# 參數
+alpha=4
+beta =3
+
+
+f=function(y) {
+  return(60 * y^3 * (1 - y)^2)
+}
+
+# cdf
+cdf_beta <- function(y) {
+  return(pbeta(y, alpha, beta))
+}
+
+target_probability <- 0.95
+result <- uniroot(function(y) cdf_beta(y) - target_probability, c(0, 1))
+
+
+value=result$root
+
+cat("the 95 percent of this distribution's value is", value, "\n")
+
